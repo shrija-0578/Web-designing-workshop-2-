ff2c626636a6cc65c66ff0e6e02c2b6cf5f00737
Shrija Lal <shrijalal6@gmail.com>
2026-02-21 15:22:01 +0530

    s_var,datatype,opretor,conditional,alert,prompt

---
+++ self/javascript.js
@@ -0,0 +1,26 @@
+console.log("This is an external JavaScript file.");
+let name = "tony";
+// can be name="tony"
+let age = 25;
+let price = 99.99;
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Salary: " + price);
+x=null;
+console.log("Value of x: " + x);
+y=undefined;
+console.log("Value of y: " + y);
+isfollowing = true; 
+console.log("Is following: " + isfollowing);
+// amazon product
+let product = {
+    name: "ball pen",
+    ratting: 4.5,
+    offer:20,
+    deal:true,
+    price: 270,
+};
+console.log(product);
+// console.log("Product Name: " + product.name);
+// console.log("Product Price: " + product.price); 
+// console.log(product.name);
