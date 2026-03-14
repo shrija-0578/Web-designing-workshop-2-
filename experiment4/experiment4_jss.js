4de33580faaf88f26669adc78db580f5182f1bf4
Shrija Lal <shrijalal6@gmail.com>
2026-03-14 17:03:27 +0530

    experiment 4 js file

---
+++ experiment4/experiment4_jss.js
@@ -0,0 +1,29 @@
+function calculateResult() {
+    let n=document.getElementById("subject").value;
+    let total=0;
+    for(let i=1;i<=n;i++)
+    {
+        let mark=parseFloat(prompt("Enter marks for subject " + i));
+        total+=mark;
+    }
+    let average=total/n;
+    let grade;
+    if(average>=90)
+        grade='A';
+    else if(average>=80)
+        grade='B';
+    else if(average>=70)    
+        grade='C';
+    else if(average>=60)
+        grade='D';
+    else
+        grade='F';
+    let result;
+    if(average>=60)
+        result="Pass";
+    else
+        result="Fail";
+
+    document.getElementById("result").innerHTML = "total marks: " + total + "<br> average marks: " + average + "<br> grade: " + grade + "<br> result: " + result;
+
+}
